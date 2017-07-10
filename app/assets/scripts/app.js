cb12a586dec563707e7f615245c754e344b49ecc
Palzalus <pasm28@outlook.com>
2017-07-10 07:15:30 +0200

    Made header sticky and added smooth scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/Stickyheader';
 import $ from 'jquery';
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "70%");
-new RevealOnScroll($('.testimonial'), "70%");
+new RevealOnScroll($('.testimonial'), "85%");
+let stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,64 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+	constructor() {
+		this.siteHeader = $('.site-header');
+		this.headerTrigger = $('.large-hero__subtitle');
+		this.createHeaderWaypoint();
+		this.pageSections = $('.page-section');
+		this.headerLinks = $('.primary-nav a');
+		this.createPageSectionWaypoints();
+		this.addSmoothScroll();
+	}
+
+	addSmoothScroll() {
+		this.headerLinks.smoothScroll({
+			speed: 1800
+		});
+	}
+
+	createHeaderWaypoint() {
+		let that = this;
+		new Waypoint({
+			element: this.headerTrigger[0],
+			handler: function() {
+				that.siteHeader.toggleClass('site-header--dark');
+			}
+		});
+	}
+
+	createPageSectionWaypoints() {
+		let that = this;
+		this.pageSections.each(function() {
+			let currentpagesection = this;
+
+			new Waypoint({
+				element: currentpagesection,
+				handler: function(direction) {
+					if (direction == "down") {
+						let matchingHeaderLink = currentpagesection.getAttribute('data-matching-link');
+						that.headerLinks.removeClass('is-current-link');
+						$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset: "25%"
+			});
+
+			new Waypoint({
+				element: currentpagesection,
+				handler: function(direction) {
+					if (direction == "up") {
+						let matchingHeaderLink = currentpagesection.getAttribute('data-matching-link');
+						that.headerLinks.removeClass('is-current-link');
+						$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset: "-25%"
+			});
+		});
+	}
+}
+
+export default StickyHeader;
